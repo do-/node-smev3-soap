57033300f4979b7d5ccf09b04b2dad945f3f4f4f
Dmitry Ovsyanko <do_@pochta.ru>
2022-12-17 11:42:06 +0300

    + getResponse

--- test/test.js
+++ test/test.js
@@ -1,11 +1,13 @@
 const {SmevSoap} = require ('../')
 
 const s = new SmevSoap ({
-	declaration: null,
-	header: ''
+//	declaration: null,
+//	header: ''
 })
 
 console.log (s.ack ('9552f341-4b2b-4cb3-b0b5-fea58fa165e1', {
 	Id: 1,
 	accepted: false,
 }))
+
+console.log (s.getResponse ())
