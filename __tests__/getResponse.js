7dfd769276e4b06d80f675aa076f7c748b44cb24
Dmitry Ovsyanko <do_@pochta.ru>
2022-12-18 13:28:12 +0300

    getResponse test code reorganized

--- __tests__/getResponse.js
+++ __tests__/getResponse.js
@@ -1,91 +1,63 @@
 const {SmevSoap}  = require ('../')
-const {XMLParser} = require ('xml-toolkit')
+const {getBody}   = require ('./lib/soap')
+const {orUndefined} = require ('./lib/o')
 
-const p = new XMLParser (), parse = xml => p.process (xml).detach ()
-
-const NS_SOAP = "http://schemas.xmlsoap.org/soap/envelope/"
 const NS_SMEV = "urn://x-artefacts-smev-gov-ru/services/message-exchange/types/1.1"
 const NS_SMEV_BASIC = "urn://x-artefacts-smev-gov-ru/services/message-exchange/types/basic/1.1"
-const XML_DECL = '<?xml version="1.0" encoding="UTF-8"?>'
+
+const getTs = dom => dom.children [0].children [0].children
+
+const toBe = (ox = {}) => {
+
+	if (!('Id' in ox)) ox.Id = 'U9552f341-4b2b-4cb3-b0b5-fea58fa165e1'
+	if (!('Timestamp' in ox)) ox.Timestamp = new Date ()
+
+	return {
+		"localName": "GetResponseRequest",
+		"namespaceURI": NS_SMEV,
+		"attributes": {},
+		"children": [
+			{
+				"localName": "MessageTypeSelector",
+				"namespaceURI": NS_SMEV_BASIC,
+				"attributes": {
+				"Id": ox.Id
+			},
+			"children": [
+					{
+						"localName": "Timestamp",
+						"namespaceURI": NS_SMEV_BASIC,
+						"attributes": {},
+						"children": [
+							ox.Timestamp.toJSON ()
+						]
+					}
+				]
+			}
+		]
+	}
+
+}
 
 function t (os, ox) {
 
 	test (JSON.stringify ([os, ox]), () => {
 	
-		const s = new SmevSoap (os)
+		const xml = new SmevSoap (os).getResponse (ox)
+		
+		const domAsIs = getBody (xml, os)
+		const domToBe = toBe (ox)
+		
+		{
+			const tsAsIs = getTs (domAsIs)
+			const tsToBe = getTs (domToBe)
 
-		const xml = s.getResponse (ox), asis = parse (xml)
+			expect (new Date (tsToBe [0]) - new Date (tsAsIs [0])).toBeLessThan (1000)
 
-		if (!os) os = {}
-
-		if (!ox) ox = {}
-		if (!('Id' in ox)) ox.Id = 'U9552f341-4b2b-4cb3-b0b5-fea58fa165e1'
-		if (!('Timestamp' in ox)) ox.Timestamp = new Date ()
-
-		const ts = asis.children.find (i => i.localName === 'Body').children [0].children [0].children [0].children
-
-		expect (new Date () - new Date (ts [0])).toBeLessThan (1000)
-
-		ts [0] = ox.Timestamp.toJSON ()
-
-		const tobe = {
-		  "localName": "Envelope",
-		  "namespaceURI": NS_SOAP,
-		  "attributes": {},
-		  "children": [
-			{
-			  "localName": "Body",
-			  "namespaceURI": NS_SOAP,
-			  "attributes": {},
-			  "children": [
-				{
-				  "localName": "GetResponseRequest",
-				  "namespaceURI": NS_SMEV,
-				  "attributes": {},
-				  "children": [
-					{
-					  "localName": "MessageTypeSelector",
-					  "namespaceURI": NS_SMEV_BASIC,
-					  "attributes": {
-						"Id": ox.Id
-					  },
-					  "children": [
-						{
-						  "localName": "Timestamp",
-						  "namespaceURI": NS_SMEV_BASIC,
-						  "attributes": {},
-						  "children": [
-							ox.Timestamp.toJSON ()
-						  ]
-						}
-					  ]
-					}
-				  ]
-				}
-			  ]
-			}
-		  ]
+			tsToBe [0] = tsAsIs [0]
 		}
 
-		if ('header' in os) tobe.children.unshift ({
-			"localName": "Header",
-			"namespaceURI": NS_SOAP,
-			"attributes": {},
-			"children": [os.header].filter (s => s !== '')
-		})
-
-		expect (asis).toStrictEqual (tobe)
-
-		if ('declaration' in os && os.declaration === null) {
-
-			expect (xml).toMatch (/^<\w+:Envelope\s/)
-
-		}
-		else {
-
-			expect (xml.indexOf (XML_DECL)).toBe (0)
-
-		}
+		expect (domAsIs).toStrictEqual (domToBe)
 
 	})
 	
@@ -107,17 +79,18 @@ for (const Id of [
 			'1'
 		]) {
 		
-			let os = {header, declaration: null}
-			for (const k in os) if (os [k] === undefined) delete os [k]
-			let oss = [os]; if (Object.keys (os).length === 0) oss.push (undefined)
+			for (const declaration of [
+				undefined,
+				null,
+			]) {
 
-			let ox = {Id, Timestamp}
-			for (const k in ox) if (ox [k] === undefined) delete ox [k]
-			let oxs = [ox]; if (Object.keys (ox).length === 0) oxs.push (undefined)
+				for (const os of orUndefined ({header, declaration}))
 
-			for (const _os of oss) 
-				for (const _ox of oxs) 
-					t (_os, _ox)
+					for (const ox of orUndefined ({Id, Timestamp}))
+
+						t (os, ox)
+
+			}
 		
 		}
 		
